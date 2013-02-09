2690f009dd700fa5a5438d3b95036eee18aeec17
thecalvinchan <calvin.chan.h@gmail.com>
2013-02-09 07:02:22 -0800

    Connected template with gCal

--- scripts/gcal.js
+++ scripts/gcal.js
@@ -1,12 +1,13 @@
-    var clientId = '823704617519.apps.googleusercontent.com';
-	var apiKey = 'AIzaSyD6B1gCukBc6Hudi0oNLXNZaCYSg1pU_MU';
-	var scopes = 'https://www.googleapis.com/auth/calendar';
-	var calid = 'g42kio0ms52em9nt39sjoulh7s@group.calendar.google.com';
+var clientId = '823704617519.apps.googleusercontent.com';
+		var apiKey = 'AIzaSyD6B1gCukBc6Hudi0oNLXNZaCYSg1pU_MU';
+		var scopes = 'https://www.googleapis.com/auth/calendar';
+		var calid = 'g42kio0ms52em9nt39sjoulh7s@group.calendar.google.com';
     var evar = new Array();
+
+    var eid = new Array();
 
 		
 		// due to the way the google API works, these access codes only work for me. replace with your own if you want to run off a local server
-
 
       // Use a button to handle authentication the first time. 
       function handleClientLoad() {
@@ -17,7 +18,6 @@
       function checkAuth() {
         gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
       }
-
 
       function handleAuthResult(authResult) {
         var authorizeButton = document.getElementById('authorize-button');
@@ -42,46 +42,36 @@
   		gapi.client.load('calendar', 'v3', function() {
     	var request = gapi.client.calendar.events.list({ 'calendarId': calid });
 
-    request.execute(function(resp) {
-      for (var i = 0; i < resp.items.length; i++) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(resp.items[i].summary));
-        evar[i] = resp.items[i].summary;
-        desc[i] = resp.items[i].description;
-      }
-    });
-  });
-}
+	    request.execute(function(resp) {
+	      for (var i = 0; i < resp.items.length; i++) {
+	        var li = document.createElement('li');
+	        li.appendChild(document.createTextNode(resp.items[i].summary));
+	        evar[i] = resp.items[i].summary;
+	        console.log(evar[i]);
+	        eid[i] = resp.items[i].id;
+	        console.log(eid[i]);
+	      }
+	    });
+	    var requestDesc = gapi.client.calendar.events.get({ 'calendarId': calid , 'eventId': 'sssjtb43u55ek0uidvh9u16f20'});
+		requestDesc.execute(description = function(resp) {return resp.description;});
+	  })};
 
-$(document).ready(function(){
-	for(i=0;i<evar.length;i++)
-	{
-		var parsedWords = new Array();
-		var curWord="";
-		for(j=0;i<desc.length;j++)
-		{
-			if(desc[i][j]=='t'||desc[i][j]=='_')
-			{	continue;}
-			else if (desc[i][j]=='%')
-			{
-				parsedWords.push(curWord);
-				continue;
-			}
-			else
-			{
-				curWord=curWord+desc[i][j];
-			}
-		}
-	}
-		$('#listTasks').append('
-		<div class="miletask five columns">
-        <div class="check">
-        <input type="checkbox" />
-        <label>Done!</label>
-      	</div>
-        <div class="taskdata">
-        <div class="taskdesc">'+parsedWords[x]+'</div>
-      	</div>
-      	</div>');
-	
-});
+	  var parsedWords=new Array;
+	  var curWord="";
+	  var i = 0;
+	  while (i<description.length)
+	  {
+	  	if (description[i]=='%'&&description[i+1]=='t'&&description[i+2]=='_')
+	  	{	parsedWords.push(curWord);
+	  		curWord="";
+	  		i=i+2;
+	  		continue;}
+	  	else 
+	  		curword=curword+description[i];
+	  	i++;
+	  }
+	  console.log(parsedWords);
+
+
+
+
