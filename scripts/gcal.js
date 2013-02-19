8297b0795b03976737c6b0d7fa00816a9b627deb
jwu <jwu@efunda.com>
2013-02-18 21:27:17 -0800

    bringing up to parity

--- scripts/gcal.js
+++ scripts/gcal.js
@@ -3,7 +3,7 @@
 		var clientId = '823704617519.apps.googleusercontent.com';
 		var apiKey = 'AIzaSyD6B1gCukBc6Hudi0oNLXNZaCYSg1pU_MU';
 		var scopes = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
-		var calid = '56ls6vo41r56kpvf0ng8dkm8hk@group.calendar.google.com';
+		var calid = 'g42kio0ms52em9nt39sjoulh7s@group.calendar.google.com';
 		//evar event titles
     	var evar = new Array();
     	//event id 
