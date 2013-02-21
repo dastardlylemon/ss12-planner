336f94eac53fda1519f723e71a2728e265e33e97
thecalvinchan <calvin.chan.h@gmail.com>
2013-02-20 17:06:56 -0800

    trying to fix syncerrors

--- scripts/gcal.js
+++ scripts/gcal.js
@@ -26,7 +26,7 @@
 	          	"name":resp.given_name,
 	          	"id":resp.id
 	          }
-	          loadEvent(eid);
+	          eid();
 	          //$('#header').append('<h5>Welcome '+user.name+'! Your email address is '+user.email+'.</h5>');
 	        });
 	      });
